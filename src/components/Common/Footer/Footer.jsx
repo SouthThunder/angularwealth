04e0759b26d657cfdfd4601a2af69bedd3d67235
Luis Alejandro Vera Moreno <alejandrovemo@gmail.com>
2023-11-06 16:08:40 -0500

    added responsive footer css

--- src/components/Common/Footer/Footer.jsx
+++ src/components/Common/Footer/Footer.jsx
@@ -1,10 +1,109 @@
-
-import React from 'react';
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
   return (
     <footer>
-      <p>This is the footer.</p>
+      <div className="sb-footer section-padding">
+        <div className="sb-footer-links">
+          <div className="sb-footer-links-div">
+            <h4>For Business</h4>
+            <a href="/test">
+              <p>Employer</p>
+            </a>
+            <a href="/health">
+              <p>Health Plan</p>
+            </a>
+            <a href="/individual">
+              <p>Individual</p>
+            </a>
+          </div>
+
+          <div className="sb-footer-links-div">
+            <h4>Resources</h4>
+            <a href="/testing">
+              <p>Testing</p>
+            </a>
+            <a href="/testing">
+              <p>Testing</p>
+            </a>
+            <a href="/testing">
+              <p>Testing</p>
+            </a>
+            <a href="/testing">
+              <p>Testing</p>
+            </a>
+          </div>
+          <div className="sb-footer-links-div">
+            <h4>Partners</h4>
+            <a href="/employers">
+              <p>Swing tech</p>
+            </a>
+          </div>
+          <div className="sb-footer-links-div">
+            <h4>Company</h4>
+            <a href="/testing2">
+              <p>testing 2</p>
+            </a>
+            <a href="/testing2">
+              <p>testing 2</p>
+            </a>
+            <a href="/testing2">
+              <p>testing 2</p>
+            </a>
+            <a href="/testing2">
+              <p>testing 2</p>
+            </a>
+          </div>
+          <div className="sb-footer-links-div">
+            <h4>Comming soon on</h4>
+            <div className="socialmedia">
+              <p>
+                <img src="./facebook.png" alt="" />
+              </p>
+              <p>
+                <img src="./instagram.png" alt="" />
+              </p>
+              <p>
+                <img src="./linkedin.png" alt="" />
+              </p>
+              <p>
+                <img src="./twitter.png" alt="" />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className="sb-footer-below">
+        <div className="sb-footer-copy">
+          <p>
+            @{new Date().getFullYear()} Angular Wealth. All Rights Reserved.
+          </p>
+        </div>
+        <div className="sb-footer-below-links">
+          <a href="/terms">
+            <div>
+              <p>Terms & conditions</p>
+            </div>
+          </a>
+          <a href="/terms">
+            <div>
+              <p>Privacy</p>
+            </div>
+          </a>
+          <a href="/terms">
+            <div>
+              <p>Security</p>
+            </div>
+          </a>
+          <a href="/terms">
+            <div>
+              <p>Cookie Declaration</p>
+            </div>
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
--- src/components/Common/Header/Header.jsx
+++ src/components/Common/Header/Header.jsx
@@ -1,34 +1,41 @@
-
-import React, { useRef } from 'react';
-import {FaBars, FaTimes} from 'react-icons/fa';
-import './Header.css';
+import React, { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "./Header.css";
 
 function Header() {
-  const navRef = useRef()
+  const navRef = useRef();
 
-  const showNavBar= () => {
-    navRef.current.classList.toggle('responsive_nav')
-  }
+  const showNavBar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
   return (
-      <header>
-        <nav ref={navRef}>
-          <h1>Angular Wealth</h1>
-          <ul>
-            <li><a className='active' href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-            <button className='nav-btn nav-close-btn' onClick={showNavBar}>
-              <FaTimes/>
-            </button>
-          </ul>
-        </nav>
-        <div className='burger-menu'>
-          <button className='nav-btn' onClick={showNavBar}>
-            <FaBars/>
+    <header>
+      <h1>Angular Wealth</h1>
+      <nav ref={navRef}>
+        <ul>
+          <li>
+            <a className="active" href="/">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/">About</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+          <button className="nav-btn nav-close-btn" onClick={showNavBar}>
+            <FaTimes />
           </button>
-        </div>
-      </header>
+        </ul>
+      </nav>
+      <div className="burger-menu">
+        <button className="nav-btn" onClick={showNavBar}>
+          <FaBars />
+        </button>
+      </div>
+    </header>
   );
 }
 
