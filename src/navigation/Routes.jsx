d533b64828c058e69bd25b265b6ed92c569470d7
Luis Alejandro Vera Moreno <alejandrovemo@gmail.com>
2023-11-19 11:55:07 -0500

    feature: added protected routes

--- src/navigation/Routes.jsx
+++ src/navigation/Routes.jsx
@@ -1,40 +1,26 @@
 // Routes.js
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Home} from "../components/Home/Home";
-import {Login} from '../components/Login/Login';
-import {Register} from '../components/Register/Register';
+import { Home } from "../components/Home/Home";
+import { Login } from "../components/Login/Login";
+import { Register } from "../components/Register/Register";
 import { Saldo } from "../components/Saldo/Saldo";
-import {Estadisticas} from "../components/Estadisticas/Estadisticas";
-import {Tarjetas} from "../components/Tarjetas/Tarjetas";
-
-// const PrivateRoute = ({ component: Component, authenticated }) => (
-//   <Route
-//     render={(props) =>
-//       authenticated ? (
-//         <Component {...props} />
-//       ) : (
-//         <Navigate to="/login" />
-//       )
-//     }
-//   />
-// );
+import { Estadisticas } from "../components/Estadisticas/Estadisticas";
+import { Tarjetas } from "../components/Tarjetas/Tarjetas";
+import PrivateRoutes from "../utils/PrivateRoute";
 
 const Routing = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/>
-        <Route path="/inicio" element={<Saldo/>}/>
-        <Route path="/estadisticas" element={<Estadisticas/>}/>
-        <Route path="/tarjetas" element={<Tarjetas/>}/>
-        {/* <PrivateRoute
-          path="/debt"
-          component={Saldo}
-          authenticated={authenticated}
-        /> */}
+        <Route exact path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path="/inicio" element={<Saldo />} />
+          <Route path="/estadisticas" element={<Estadisticas />} />
+          <Route path="/tarjetas" element={<Tarjetas />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
